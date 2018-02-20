73e0a0f1c1ab0ded4a4be076322e9a04169b10ed
Elad Keyshawn <elad.keyshawn@gmail.com>
2018-02-20 19:45:40 +0200

    trial of stack overflow

--- index.js
+++ index.js
@@ -6,22 +6,58 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require("./config.json").server;
 const db = require('./db');
-
+const auth = require('./auth');
 
 const PORT = process.env.PORT || config.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
 
+let Stack = [];
+
+const IpMonitor = {
+    "::ffff:10.100.102.6": {
+        count: 0,
+        expiry: "234235435345"
+    }
+};
+
+// setInterval(()=> {
+//     Stack = [];
+// },100000);
+
+
+app.use('/',(req,res) => {
+    const payload = req.body;
+    Stack.push(payload);
+})
 
 app.get('/hello', (req, res) => {
 
-  db.query("SELECT * FROM users").then(result => {
-      res.send("Hello world!");
-  });
+    db.query("SELECT * FROM users").then(result => {
+        res.send("Hello world!");
+    });
+
 
 
 });
 
 
-app.listen( PORT , () => console.log("listening on port", PORT ));
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    auth.signup(req.body)
+      .then((response) => {
+        res.json(response);
+      })
+});
+
+app.post('/login', (req, res) => {
+
+    console.log(req.body);
+    auth.login(req.body)
+      .then((response) => {
+        res.json(response);
+      })
+});
+
+app.listen(PORT, () => console.log("listening on port", PORT));
