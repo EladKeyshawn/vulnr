64e3c3327a0ad890b5f93a39bb929de8da940d0e
Elad Keyshawn <elad.keyshawn@gmail.com>
2018-03-16 15:41:30 +0200

    add eslint example

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
-const assert = require('chai').assert;
+const { assert } = require('chai');
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const config = require("./config.json").server;
+const config = require('./config.json').server;
 const auth = require('./lib/auth');
 const levels = require('./lib/levels');
 const constants = require('./lib/constants');
 const Ddos = require('./lib/ddos');
-const ddsosMsg = constants.NICE_ONE_MSG + " here: sfl34lkf";
-const ddos = new Ddos({silent:true,burst: 30, limit: 45,responseStatus:200,errormessage: ddsosMsg});
+
+const ddsosMsg = `${constants.NICE_ONE_MSG} here: sfl34lkf`;
+const ddos = new Ddos({
+  silent: true, burst: 30, limit: 45, responseStatus: 200, errormessage: ddsosMsg,
+});
 const crypto = require('./lib/crypto');
 
 const PORT = process.env.PORT || config.PORT;
@@ -20,46 +24,47 @@ app.use(cors());
 
 
 app.get('/hello', (req, res) => {
-     res.send("Hello world!");
+  res.send('Hello world!');
 });
 
-app.get('/level/validate', (req,res)=>{
-    const level = req.query.level, solution = req.query.sol;
-    assert.isOk(level, 'level # must be provided for validation');
-    // assert.isOk(solution,'solution must be provided for validation');
+app.get('/level/validate', (req, res) => {
+  const { level } = req.query;
+  const solution = req.query.sol;
+  assert.isOk(level, 'level # must be provided for validation');
+  // assert.isOk(solution,'solution must be provided for validation');
 
-    levels.validate(level,solution)
-      .then(result => {
-          res.json(result);
-      })
+  levels.validate(level, solution)
+    .then((result) => {
+      res.json(result);
+    });
 });
 
-app.post('/ddos',ddos.express);
+app.post('/ddos', ddos.express);
 
 app.post('/signup', (req, res) => {
-    auth.signup(req.body)
-      .then((response) => {
-        res.json(response);
-      })
+  auth.signup(req.body)
+    .then((response) => {
+      res.json(response);
+    });
 });
 
 app.post('/login', (req, res) => {
-    auth.login(req.body)
-      .then((response) => {
-        res.json(response);
-      })
+  auth.login(req.body)
+    .then((response) => {
+      res.json(response);
+    });
 });
 
-app.get('/enc', (req,res)=>{
-    const msg = req.query.msg;
-    assert.isOk(msg, 'level # must be provided for validation');
-    res.json(crypto.enc(msg))
+app.get('/enc', (req, res) => {
+  const { msg } = req.query;
+  assert.isOk(msg, 'level # must be provided for validation');
+  res.json(crypto.enc(msg));
 });
 
-app.get('/takeover',  (req,res)=> {
-    levels.checkDomainTakover().then(result => {
-        res.json(result);
-    })
+app.get('/takeover', (req, res) => {
+  levels.checkDomainTakover().then((result) => {
+    res.json(result);
+  });
 });
 
-app.listen(PORT, () => console.log("listening on port", PORT));
+app.listen(PORT, () => console.log('listening on port', PORT));
--- test.js
+++ test.js
@@ -1,22 +1,21 @@
 const rp = require('request-promise');
 
 
-for (let i = 0; i < 5000; i++) {
-    rp({
-      //  url: "http://localhost:8000/ddos",
-	url: "https://vulnr.hack-au.com/ddos",       
- method: "POST",
+for (let i = 0; i < 5000; i += 1) {
+  rp({
+    //  url: "http://localhost:8000/ddos",
+    url: 'https://vulnr.hack-au.com/ddos',
+    method: 'POST',
 
-        json: true // Automatically stringifies the body to JSON
-    }).then((response) => {
-        if(response) {
-            console.log(response);
-        }
-      }).catch(err => {
-        if(err) {
-            // console.log(err);
-        }
-    })
+    json: true, // Automatically stringifies the body to JSON
+  }).then((response) => {
+    if (response) {
+      console.log(response);
+    }
+  }).catch((err) => {
+    if (err) {
+      // console.log(err);
+    }
+  });
 }
-
 
